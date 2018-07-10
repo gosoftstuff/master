4728338ad826c90fc87091209407f404e5ae28dc
mtnbkr <dklinefelter1@gmail.com>
2018-07-10 14:04:45 -0400

    [ADD] Changed init

--- customer_num/__manifest__.py
+++ customer_num/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "/home/darren/softstuff/master/CustomerNum",
+    'name': "Change Customer Number",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        I also changed this""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Soft Stuff Distributors",
+    'website': "http://www.gosoftstuff.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
